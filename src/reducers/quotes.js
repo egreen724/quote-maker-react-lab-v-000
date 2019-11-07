c960d6278ad6a0cddca0d803c814886f1c7f740b
Erin Green <erin.y.green@gmail.com>
2019-11-07 19:42:37 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -2,9 +2,8 @@ export default function quotes(state = [], action) {
  
   switch (action.type) {
     case "ADD_QUOTE":
-      return {
-        ...state, {...state.quotes, action.quote }
-      };
+      return state.concat(action.quote);
+
 
     case "REMOVE_QUOTE":
       return state.filter(quote => quote.id !== action.quoteId);
